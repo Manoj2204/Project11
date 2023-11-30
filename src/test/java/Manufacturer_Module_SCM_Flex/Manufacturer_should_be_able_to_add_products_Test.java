8a9de53af6adcfa24e335a617bd321dcdcc06c2b
Manoj2204 <mahtomanojkumar824@gmail.com>
2023-11-30 17:24:03 +0530

    second commit

---
+++ src/test/java/Manufacturer_Module_SCM_Flex/Manufacturer_should_be_able_to_add_products_Test.java
@@ -0,0 +1,71 @@
+package Manufacturer_Module_SCM_Flex;
+
+import java.io.IOException;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import com.POM.LoginPage;
+import com.POM.ManufacturerAddproductPage;
+import com.POM.ManufacturerHomePage;
+import com.crm.GenericUtils.BaseClass;
+import com.crm.GenericUtils.ExcelUtils;
+import com.crm.GenericUtils.FileUtils;
+import com.crm.GenericUtils.WebDriverUtils;
+
+@Listeners(com.crm.GenericUtils.ListnerImplementation.class)
+public class Manufacturer_should_be_able_to_add_products_Test extends BaseClass
+{
+	@Test
+public void test() throws IOException
+{
+	// Create objects for Generic Utility			
+	FileUtils fileutils = new FileUtils();		
+	WebDriverUtils webdriverutils= new WebDriverUtils(); 
+	ExcelUtils excelutils = new ExcelUtils();
+	
+	//String URL=fileutils.readDataFromPropertyFile("url");
+	String MUSERNAME=fileutils.readDataFromPropertyFile("manufacturerusername");
+	String MPASSWORD=fileutils.readDataFromPropertyFile("manufacturerpassword");
+	String MLOGINTYPE=fileutils.readDataFromPropertyFile("Manufacturerlogintype");
+		
+	LoginPage loginpage= new LoginPage(driver);
+	ManufacturerHomePage mHomepage = new ManufacturerHomePage(driver);
+	ManufacturerAddproductPage mAddproductPage= new ManufacturerAddproductPage(driver);
+	
+	// Enter username and password and click on login button
+	loginpage.EnterUsernameAndPassword(MUSERNAME, MPASSWORD);
+	loginpage.EnterLoginType(MLOGINTYPE);
+	loginpage.ClickOnLoginButton();
+	
+	// Compare the actual and expected page
+	String actualtitle = driver.getTitle();
+	String expectedtitle="Manufacturer: Home";
+	Assert.assertEquals(actualtitle, expectedtitle);
+	System.out.println("Homepage is displayed... and its verified with title");
+	
+	// Click on add product link
+	mHomepage.ClickOnaddProductLink();
+	
+	String actualaddproducttitle = driver.getTitle();
+	String expectedproductpagetitle= "Add Product";
+	Assert.assertEquals(actualaddproducttitle, expectedproductpagetitle);
+	System.out.println("addProduct page is displayed... and its verified with title");
+	
+	
+	mAddproductPage.EnterdataIntoAddTextField(excelutils.readMultipleData("Sheet2"), driver);
+	mAddproductPage.getEnableradioButton().click();
+	mAddproductPage.getAddButton().click();
+	
+	// Handle the confirmation popup
+	String ctext= driver.switchTo().alert().getText();
+	System.out.println(ctext);
+	webdriverutils.acceptAlertPopup(driver);
+   }
+
+}
